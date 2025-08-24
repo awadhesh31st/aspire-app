587954eca6c65463b547061141e730aa02e36c33
Awadhesh Kumar <awadhesh@Awadheshs-MacBook-Air.local>
2025-08-24 16:49:29 +0530

    code push

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,7 +1,11 @@
 import { navigation } from '../../content'
 import { memo, useMemo } from 'react'
+import { useIsMobile } from '../../hooks/useIsMobile'
+import { aspireLogo } from '../../assets'
 
 const NavigationComponent = memo(() => {
+  const isMobile = useIsMobile()
+
   const navigationItems = useMemo(() => {
     return navigation?.map((item, index) => {
       const isActive = '/cards' === item.path
@@ -9,7 +13,7 @@ const NavigationComponent = memo(() => {
       return (
         <div
           key={item?.path || index}
-          className={`flex cursor-pointer flex-col items-center justify-start gap-0.5 py-3 md:flex-row ${isActive ? 'text-brand-green grayscale-0' : 'text-neutral-gray grayscale'}`}
+          className={`flex cursor-pointer flex-col items-center justify-start gap-0.5 py-3 md:w-full md:flex-row md:gap-2 ${isActive ? 'text-brand-green grayscale-0' : 'text-neutral-gray grayscale'}`}
         >
           {item?.icon &&
             (typeof item.icon === 'string' ? (
@@ -33,7 +37,15 @@ const NavigationComponent = memo(() => {
 
   return (
     <div className="fixed bottom-0 z-[100] w-full justify-center bg-white px-4 shadow-2xl md:relative md:h-full md:w-auto md:border-0 md:bg-brand-dark-navy md:px-0">
-      <nav className="flex w-full flex-row items-end justify-between gap-4 md:flex-col md:gap-0">
+      {!isMobile && (
+        <div className="flex flex-col gap-4 p-8">
+          <img src={aspireLogo} className="h-auto w-24" alt="aspire" />
+          <p className="text-xs text-neutral-cool-gray">
+            Trusted way of banking for 3,000+ SMEs and startups in Singapore
+          </p>
+        </div>
+      )}
+      <nav className="flex w-full flex-row items-end justify-between gap-4 md:flex-col md:gap-5 md:p-8">
         {navigationItems}
       </nav>
     </div>
